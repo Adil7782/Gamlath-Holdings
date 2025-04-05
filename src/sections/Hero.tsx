e523b9b9f160ef18a12328ecd36883b9638453ea
Your Name <adilsaaly@gmail.com>
2025-04-05 11:58:35 +0530

    cog done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,17 @@
 import ArrowIcon from "@/assets/arrow-right.svg";
 import Image from "next/image";
 import cogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
 export const Hero = () => {
   return (
     <>
-      <div className="p-5 pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
-        <div className="md:flex items-center">
+      <div className="p-5 pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
+        <div className="md:flex">
         <div className="md:w-[478px]">
           <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
             Version 2.0 is here
           </div>
-          <h1 className="font-bold tracking-tighter text-5xl bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+          <h1 className="font-bold tracking-tighter text-5xl md:text-7xl bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
             Pathway to Productivity
           </h1>
           <p className="text-xl tracking-tight text-[#010D3E] mt-6">
@@ -27,8 +28,8 @@ export const Hero = () => {
             </button>
           </div>
         </div>
-        <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-          <Image src={cogImage} alt="cog" className="md:absolute md:h-full md:w-auto md:max-w-none md:left-6" />
+        <div className="mt-20 md:mt-0 md:h-[648px]  md:flex-1 relative">
+          <Image src={cogImage} alt="cog"  className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6"/>
         </div>
         </div>
       </div>
