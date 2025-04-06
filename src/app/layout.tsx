262223463f2e55f35848631f8db567acb7ab6a90
Your Name <adilsaaly@gmail.com>
2025-04-06 23:36:40 +0530

    added compo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
-
+import { Analytics } from '@vercel/analytics/next';
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className="relative scroll-smooth">
       <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE] scroll-smooth")}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
